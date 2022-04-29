e2c3ba966722aaab1106298aa018386a3b5f64fa
abdelrahman esam <abdelrahmane792@outlook.com>
2022-04-29 16:26:53 +0200

    last updated

--- ResearchGate/Models/ErrorViewModel.cs
+++ ResearchGate/Models/ErrorViewModel.cs
@@ -5,7 +5,7 @@ namespace ResearchGate.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
+        //hello
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
