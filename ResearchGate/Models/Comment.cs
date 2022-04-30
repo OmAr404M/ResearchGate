af8a97859b87a4b66fa85de6dae3310dddcf3a18
abdelrahman esam <abdelrahmane792@outlook.com>
2022-04-30 05:15:00 +0200

    models update

---
+++ ResearchGate/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResearchGate.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Body")]
+        [Required(ErrorMessage = "Body is required")]
+        public string Body { get; set; }
+    }
+}
---
+++ ResearchGate/Models/Like.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResearchGate.Models
+{
+    public class Like
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Type")]
+        [Required(ErrorMessage = "Type is required")]
+        public string Type { get; set; }
+
+
+    }
+}
