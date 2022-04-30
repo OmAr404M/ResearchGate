db1ae3886a8fe9bc7c7b820d0438a37cb0975610
Abelrahman Essam <103298770+boody912@users.noreply.github.com>
2022-04-30 15:13:01 +0200

    Update Comment.cs

--- ResearchGate/Models/Comment.cs
+++ ResearchGate/Models/Comment.cs
@@ -14,5 +14,8 @@ namespace ResearchGate.Models
         [Display(Name = "Body")]
         [Required(ErrorMessage = "Body is required")]
         public string Body { get; set; }
+        
+        //Relationships
+        public List<Paper> Papers { get; set; }
     }
 }
