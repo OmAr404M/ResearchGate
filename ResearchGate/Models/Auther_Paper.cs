2762407447fc0a3d9c1a92c31d2f148f77dfad5b
Abelrahman Essam <103298770+boody912@users.noreply.github.com>
2022-04-30 15:09:23 +0200

    Add files via upload

---
+++ ResearchGate/Models/Auther_Paper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResearchGate.Models
+{
+    public class Auther_Paper
+    {
+        public int AutherId { get; set; }
+        public Auther Movie { get; set; }
+
+        public int PaperId { get; set; }
+        public Paper Paper { get; set; }
+    }
+}
