ea86fa3cce27722e7c2111e426c11d98f5a8e7d5
abdelrahman esam <abdelrahmane792@outlook.com>
2022-04-30 05:04:10 +0200

    latest update

---
+++ ResearchGate/Models/Paper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ResearchGate.Models
+{
+    public class Paper
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime Date { get; set; }
+       
+        
+
+    }
+}
